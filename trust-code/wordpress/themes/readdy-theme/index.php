8669db1188905f48e153b87066819d5a19b37f99
Akira Kano <a.kano1101@gmail.com>
2025-10-06 13:33:03 +0900

    記事のキャッチコピー？がおかしいが一応ブログ一覧とブログが表示されるようになった。

--- trust-code/wordpress/themes/readdy-theme/index.php
+++ trust-code/wordpress/themes/readdy-theme/index.php
@@ -1,14 +1,2 @@
 <?php
-// Readdy Theme main template
-get_header();
-
-if ( have_posts() ) {
-  while ( have_posts() ) {
-    the_post();
-    the_content();
-  }
-} else {
-  echo '<p>No content found</p>';
-}
-
-get_footer();
+// Silence is golden.
